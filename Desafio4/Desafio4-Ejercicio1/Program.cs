9f1203b61786d27e1bf1d8341c6c9486973e12c1
Mariano Chun <mchun@andreani.com>
2023-01-10 17:25:18 -0300

    Add resolucion desafio 4

---
+++ Desafio4/Desafio4-Ejercicio1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio4_Ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] origen = { 1002, 104, 309, 500};
+            int[] destino = new int[origen.Length];
+
+            int indiceInvertidoOrigen = origen.Length - 1;
+
+            Console.WriteLine("Indices y valores nuevo vector (destino)\n");
+            for(int i = 0; i < destino.Length; i++)
+            {
+                destino[i] = origen[indiceInvertidoOrigen];
+                Console.WriteLine($"Vector destino: Indice: {i} con valor {destino[i]}");
+                
+                indiceInvertidoOrigen--;
+            }
+
+            Console.WriteLine("\nPresione una tecla para finalizar...");
+            Console.ReadKey();      
+        }
+    }
+}
---
+++ Desafio4/Desafio4-Ejercicio2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio4_Ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] facturacionAnual =
+            {
+                // 1er trimestre
+                1000,
+                2000,
+                1500,
+
+                // 2do trimestre
+                5000,
+                3500,
+                500,
+
+                // 3er trimestre
+                6000,
+                3000,
+                1200,
+
+                //4to trimestre
+                200,
+                250,
+                300
+            };
+
+            int[] facturacionTrimestres = new int[4];
+            // El indice de facturacion trimestral aumenta en indice mes = 2 (mes 3), 5 (mes 6), 8 (mes 9) y 11 (mes 12)
+            int indiceFacturacionTrimestral = 0;
+
+            int sumaFacturacionTrimestral = 0;
+
+            for(int indiceMes = 1; indiceMes <= facturacionAnual.Length; indiceMes++)
+            {
+                sumaFacturacionTrimestral += facturacionAnual[indiceMes-1];
+                
+                if (indiceMes % 3 == 0)
+                {
+                    facturacionTrimestres[indiceFacturacionTrimestral] = sumaFacturacionTrimestral;
+                    sumaFacturacionTrimestral = 0;
+                    indiceFacturacionTrimestral++;
+                }
+            }
+
+            // Imprimo los valores del año
+            Console.WriteLine("Durante el año, se facturo lo siguiente\n");
+
+            foreach (int facturacionMensual in facturacionAnual)
+            {
+                Console.WriteLine($"Se facturó ${facturacionMensual} durante el mes {Array.IndexOf(facturacionAnual, facturacionMensual)+1}");
+            }
+
+            // Imprimo los valores trimestrales
+            Console.WriteLine("\nDurante cada trimestre del año, se facturo lo siguiente\n");
+
+            foreach (int facturacionTrimestral in facturacionTrimestres)
+            {
+                Console.WriteLine($"Se facturó ${facturacionTrimestral} durante el trimestre {Array.IndexOf(facturacionTrimestres, facturacionTrimestral)+1}");
+            }
+
+
+            Console.WriteLine("\nPresione una tecla para finalizar...");
+            Console.ReadKey();
+
+        }
+    }
+}
